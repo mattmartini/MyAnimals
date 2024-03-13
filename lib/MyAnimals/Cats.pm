196ef5f958b9b07e5c3bc13a53c85826e9560b0d
Matt Martini <matt.martini@gmail.com>
2024-03-13 16:27:31 -0400

    Add purr fuction

--- lib/MyAnimals/Cats.pm
+++ lib/MyAnimals/Cats.pm
@@ -8,9 +8,11 @@ our $VERSION = '0.01';
 
 use Exporter qw( import );
 
-our @EXPORT_OK = qw( meow );
+our @EXPORT_OK = qw( meow purr );
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );  # Optional.
 
 sub meow { return "meow\n"; }
+
+sub purr { return "purr\n"; }
 
 1;
